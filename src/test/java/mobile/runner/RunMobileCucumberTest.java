c30a04b8a7b943cf0d198bb23fae0f89a1904f86
Bequnio <cartoonx65@gmail.com>
2024-11-27 02:40:23 +0400

    added allure report

--- src/test/java/mobile/runner/RunMobileCucumberTest.java
+++ src/test/java/mobile/runner/RunMobileCucumberTest.java
@@ -9,7 +9,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features", // Replace with the path to your feature files
         glue = "mobile/stepDefinitions", // Replace with the package containing your step definitions
         tags = "@mobile",
-        plugin = {"pretty", "html:target/cucumber-reports.html"} // Optional: specify plugins for report generation
+        plugin = {"io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"} // Optional: specify plugins for report generation
 )
 
 @RunWith(Cucumber.class)
